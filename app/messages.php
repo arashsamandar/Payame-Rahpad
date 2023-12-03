8761fc92b1a24362ea7396a8df3364f11443123d
arashsamandar <arash.internet@gmail.com>
2023-12-03 22:46:24 +0330

    some message

--- app/messages.php
+++ app/messages.php
@@ -12,5 +12,4 @@ class messages extends Model
     public function contents() {
         return $this->belongsTo('App\Content','content_id','id');
     }
-    // ok then here goes some change to codes :)
 }
