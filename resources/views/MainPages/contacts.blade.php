4f1c56197342bee99e20013453076b3643bab9cf
arashsamandar <arash.internet@gmail.com>
2024-05-08 00:50:35 +0330

    everything set at 1403/2/18

--- resources/views/MainPages/contacts.blade.php
+++ resources/views/MainPages/contacts.blade.php
@@ -9,8 +9,18 @@
             Contact Me :
         </h3>
         <div style="margin-left:100px;margin-right:50px;margin-bottom:30px;padding:10px;border-radius: 5px;overflow:auto;text-align: justify;">
-            Hello and Welcome to my personal website :<br>
-            my name is arash i am 35 and i am a freelancer<br>
+            <a style="font-weight: bold">Email</a> :<br>
+            <span style="font-weight: bold">
+                admin@aarash.agency.com
+            </span><br>
+            <span style="font-weight: bold">
+                arash.internet@gmail.com
+            </span><br><br>
+            <a style="font-weight: bold">Phone number : </a><br>
+            00989127364767<br><br>
+            <a style="font-weight: bold">Social Links : </a><br>
+            LinkedIn : https://www.linkedin.com/in/arash-samandar-0288b4160/<br>
+            Github : https://www.github.com/arashsamandar<br>
         </div>
     </div>
 @endsection
